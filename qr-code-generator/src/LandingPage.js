7b430694311be38846d39f6afb7f877cf6e79178
dariaar <droso00@fesb.hr>
2025-01-13 18:10:39 +0100

    landing page v2

--- qr-code-generator/src/LandingPage.js
+++ qr-code-generator/src/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Ispravan import za navigaciju
+import image1 from './slike/image1.jpg'
 
 const LandingPage = () => {
   const navigate = useNavigate();  // Korištenje useNavigate za navigaciju
@@ -9,15 +10,29 @@ const LandingPage = () => {
   };
 
   return (
-    <div style={{ textAlign: 'center', padding: '50px', backgroundColor: '#f0f8ff', minHeight: '100vh' }}>
-      <h1>Dobrodošli na naš sustav</h1>
-      <p>Molimo prijavite se kako biste nastavili.</p>
-      <button
-        onClick={handleLoginClick}
-        style={{ padding: '10px 20px', fontSize: '18px', borderRadius: '5px', backgroundColor: '#0f1c30', color: '#c2c6ce', border: 'none', cursor: 'pointer' }}
-      >
-        Prijava u sustav
-      </button>
+    <div style={{ backgroundColor: '#f0f8ff' }}>
+      <div style={{ paddingLeft: '50px', backgroundColor: '#f0f8ff', paddingTop:'20px' }}>
+        <h2>ime app</h2>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', minHeight: '100vh', padding: '50px' }}>
+        <div style={{ flex: 1, padding: '20px', textAlign: 'center' }}>
+          <h1>Dobrodošli na naš sustav</h1>
+          <p>Molimo prijavite se kako biste nastavili.</p>
+          <button
+            onClick={handleLoginClick}
+            style={{ padding: '10px 20px', fontSize: '18px', borderRadius: '5px', backgroundColor: '#0f1c30', color: '#c2c6ce', border: 'none', cursor: 'pointer' }}
+          >
+            Prijava u sustav
+          </button>
+        </div>
+        <div style={{ flex: 1, padding: '20px' }}>
+          <img 
+            src={image1} 
+            alt="Descriptive Alt Text" 
+            style={{ width: '100%', height: 'auto', borderRadius: '10px' }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
